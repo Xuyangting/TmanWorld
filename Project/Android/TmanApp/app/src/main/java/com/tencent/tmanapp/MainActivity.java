d50784616bdbc0f9d77fc0cfba5e83ce1c3993a2
timen <407708323@qq.com>
2020-01-27 17:10:06 +0800

    添加底部菜单栏

--- Project/Android/TmanApp/app/src/main/java/com/tencent/tmanapp/MainActivity.java
+++ Project/Android/TmanApp/app/src/main/java/com/tencent/tmanapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.tencent.tmanapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+import com.tencent.tmanapp.menu.DashboardFragment;
+import com.tencent.tmanapp.menu.HomeFragment;
+import com.tencent.tmanapp.menu.MessageFragment;
+
+public class MainActivity extends AppCompatActivity implements
+        HomeFragment.OnFragmentInteractionListener,
+        DashboardFragment.OnFragmentInteractionListener,
+        MessageFragment.OnFragmentInteractionListener{
+    private Fragment homeFragment;
+    private Fragment dashboardFragment;
+    private Fragment messageFragment;
+    private Fragment[] listFragment;
+    private int lastIndex;
+
+    private void initData(){
+        homeFragment = new HomeFragment();
+        dashboardFragment = new DashboardFragment();
+        messageFragment = new MessageFragment();
+        listFragment = new Fragment[]{homeFragment, dashboardFragment, messageFragment};
+        lastIndex = 0;
+        // 默认显示 home fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentView, homeFragment).show(homeFragment).commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,13 +42,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setFragment(lastIndex, 0);
+                    lastIndex = 0;
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    setFragment(lastIndex, 1);
+                    lastIndex = 1;
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    setFragment(lastIndex, 2);
+                    lastIndex = 2;
                     return true;
             }
             return false;
@@ -34,9 +62,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void setFragment(int lastIndex, int index){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.hide(listFragment[lastIndex]);
+        if (!listFragment[index].isAdded()){
+            transaction.add(R.id.fragmentView, listFragment[index]);
+        }
+        transaction.show(listFragment[index]).commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
+    }
 }
